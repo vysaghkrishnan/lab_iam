b3775522ea20b993d6cab478a55f203d0480c0e6
vysaghkrishnan <vysaghkrishnan4u@gmail.com>
2022-01-27 13:04:38 +0000

    filr

---
+++ dev.tfvars
@@ -0,0 +1,8 @@
+AWS_REGION = "eu-west-1"
+AWS_PROFILE = "default"
+
+AMIS = {
+    eu-west-1 = "11d" # US N.virginia
+}
+
+instance_type = "t2.micro"
---
+++ vars.tf
@@ -0,0 +1,23 @@
+variable "AWS_REGION" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "AWS_PROFILE" {
+  type    = string
+  default = "default"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    eu-west-1 = "ami-08edbb0e85d6a0a07" # EU Ireland
+    eu-west-2 = "ami-0fdf70ed5c34c5f52" # EU London
+    us-east-1 = "ami-083654bd07b5da81d" # US N.virginia
+  }
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
