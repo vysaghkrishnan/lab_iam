aef4b84c79029e792fd9239e282f15c0d732802b
Vysaghsrepo <87260139+vysaghkrishnan@users.noreply.github.com>
2022-02-28 11:29:08 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+
+
+
+
+
 provider "aws" {
     # access_key = "${var.AWS_ACCESS_KEY}"
     # secret_key = "${var.AWS_SECRET_KEY}"
