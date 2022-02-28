cb9b0dd8e23df1c9c132bf3a776b3dc03c9a9c9c
vysaghkrishnan <vysaghkrishnan4u@gmail.com>
2022-02-28 12:26:48 +0000

    the files

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
     profile = "default"
     region = var.AWS_REGION
-    aws_access_key_id = AKIAWBL3BEW5R4DIU4HG
-    aws_secret_access_key = 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
+    
 }
