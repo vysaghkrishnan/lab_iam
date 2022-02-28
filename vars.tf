782e112dad502a3913952d80473e3376a8cc4f74
Vysaghsrepo <87260139+vysaghkrishnan@users.noreply.github.com>
2022-02-28 11:24:29 +0000

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
-variable "AWS_REGION" {
-  type    = string
-  default = "eu-west-2"
+#variable "AWS_REGION" {
+  #type    = string
+  #fault = "eu-west-2"
 }
 
-variable "AWS_PROFILE" {
-  type    = string
-  default = "default"
+#variable "AWS_PROFILE" {
+ #type    = string
+  #default = "default"
 }
 
 variable "AMIS" {
