2426280c69277d6226322c931654214a4cdf1ed1
vysaghkrishnan <vysaghkrishnan4u@gmail.com>
2022-02-28 12:20:58 +0000

    the files

--- provider.tf
+++ provider.tf
@@ -1,4 +1,6 @@
 provider "aws" {
     profile = "default"
     region = var.AWS_REGION
+    aws_access_key_id = AKIAWBL3BEW5R4DIU4HG
+    aws_secret_access_key = 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
 }
